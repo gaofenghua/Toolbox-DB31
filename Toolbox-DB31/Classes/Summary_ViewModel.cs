425c8dd26f7a9e4c5c42aeef68cf6de7667f2500
gaofenghua <gaofenghua@gmail.com>
2019-11-26 11:31:29 +0800

    Initial Project

---
+++ Toolbox-DB31/Classes/Summary_ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Mvvm;
+using System.Collections.ObjectModel;
+
+namespace Toolbox_DB31.Classes
+{
+    class Summary_ViewModel : ViewModelBase
+    {
+        public ObservableCollection<Camera_Model> CameraList { get; set; }
+
+        public Summary_ViewModel()
+        {
+            CameraList = new ObservableCollection<Camera_Model>();
+            CameraList.Add(new Camera_Model() { Name = "test",IsSelected=true });
+            CameraList.Add(new Camera_Model() { Name = "test2", IsSelected = false });
+        }
+    }
+}
---
+++ Toolbox-DB31/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Toolbox_DB31
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : DevExpress.Xpf.Core.ThemedWindow
+    {
+        public static RoutedCommand myCommand = new RoutedCommand();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNew_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e)
+        {
+           
+
+        }
+        private void myCommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            //Actions to perform 
+           
+        }
+        private void myCommandExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            //Case of command's availability 
+            if (true)
+            {
+                e.CanExecute = true;
+            }
+        }
+    }
+}
