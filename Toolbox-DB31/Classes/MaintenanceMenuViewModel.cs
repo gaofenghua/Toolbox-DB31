1a3a47aaa3f15aa67dc76db18dcd5941a28ed09b
Irene Wang <irenew@axis.com>
2020-03-04 16:35:17 +0800

    1.fix tab click for selecting repair or maintenance 2.adjust pages and accomplish data output

--- Toolbox-DB31/Classes/MaintenanceMenuViewModel.cs
+++ Toolbox-DB31/Classes/MaintenanceMenuViewModel.cs
@@ -3,22 +3,38 @@ using DevExpress.Mvvm;
 
 namespace Toolbox_DB31.Classes
 {
-	public class MaintenanceMenuViewModel : ViewModelBase
-	{
-		public bool IsVideoMonitorEnabled = false;
-		public bool IsPerimeterAlarmEnabled = false;
-		public bool IsIntruderAlarmEnabled = false;
-		public bool IsBuildingIntercomEnabled = false;
-		public bool IsAccessManagementEnabled = false;
-		public bool IsEntranceControlEnabled = false;
-		public bool IsGuardTourEnabled = false;
-		public bool IsOthersEnabled = false;
-		public string MaintenanceRecords = string.Empty;
+    public enum MaintenanceType
+    {
+        REGULAR_MAINTENANCE = 0,
+        SPECIAL_MAINTENANCE
+    }
 
-		public enum MaintenanceStatus
-		{
-			REGULAR_MAINTENANCE = 0,
-			SPECIAL_MAINTENANCE
-		}
+    public class MaintenanceMenuViewModel : ViewModelBase
+	{
+        public MaintenanceType m_Type { get; set; }
+        public bool IsVideoMonitorEnabled { get; set; }
+        public bool IsPerimeterAlarmEnabled { get; set; }
+        public bool IsIntruderAlarmEnabled { get; set; }
+        public bool IsBuildingIntercomEnabled { get; set; }
+        public bool IsAccessManagementEnabled { get; set; }
+        public bool IsEntranceControlEnabled { get; set; }
+        public bool IsGuardTourEnabled { get; set; }
+        public bool IsOthersEnabled { get; set; }
+        public string MaintenanceRecords { get; set; }
+
+        public MaintenanceMenuViewModel()
+        {
+            m_Type = MaintenanceType.REGULAR_MAINTENANCE;
+            IsVideoMonitorEnabled = false;
+            IsPerimeterAlarmEnabled = false;
+            IsIntruderAlarmEnabled = false;
+            IsBuildingIntercomEnabled = false;
+            IsAccessManagementEnabled = false;
+            IsEntranceControlEnabled = false;
+            IsGuardTourEnabled = false;
+            IsOthersEnabled = false;
+            MaintenanceRecords = string.Empty;
+        }
+
 	}
 }
