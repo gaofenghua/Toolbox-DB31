4ed4ea7288e26374a1619871c318f1213150815f
Irene Wang <irenew@axis.com>
2020-03-04 17:47:51 +0800

    append fault repair menu

---
+++ Toolbox-DB31/Classes/FaultRepairMenuViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DevExpress.Mvvm;
+
+namespace Toolbox_DB31.Classes
+{
+    public class FaultRepairMenuViewModel
+    {
+        public FaultRepairMenuViewModel()
+        {
+            //
+        }
+    }
+}
